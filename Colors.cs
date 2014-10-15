fe690bcdc5ec044085d6f9fd09bdff08dfb8a22d
Damian Bernardi <damian.bernardi@somniumsoft.com>
2014-10-16 01:49:10 +0200

    b5

--- Colors.cs
+++ Colors.cs
@@ -1,23 +1,21 @@
 // Damian Bernardi (c) Somniumsoft
 
 using UnityEngine;
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Collections;
 
-public class ColorSettings
+public static class ColorExtensions
 {
 
-	public static string ColorToHex(Color32 color){
-		
+	//convert color to hex
+	public static string ToHex (this Color color){
+
 		return color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
 	}
-	
-	public static Color Hex(string hex){
+
+	//convert string to color
+	public static Color ToColor (this string hex){
 		
-		return new Color32(byte.Parse(hex.Substring(0,2), System.Globalization.NumberStyles.HexNumber),
-		                   byte.Parse(hex.Substring(2,2), System.Globalization.NumberStyles.HexNumber),
-		                   byte.Parse(hex.Substring(4,2), System.Globalization.NumberStyles.HexNumber), 255);
+		return new Color32(byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber),
+		                   byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber),
+		                   byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber), 255);
 	}
 }
